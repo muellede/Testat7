e241d67098b618d8076634e261542da02b2db4f2
DZ <DZ@denizs-mbp.fritz.box>
2016-01-27 17:25:58 +0100

    Tests

--- biblio-db/src/test/java/de/hrw/swep/biblio/persistence/DAOTest.java
+++ biblio-db/src/test/java/de/hrw/swep/biblio/persistence/DAOTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.File;
+import java.util.Set;
 
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
@@ -12,35 +13,39 @@ import org.junit.Before;
 import org.junit.Test;
 
 import de.hrw.swep.biblio.persistence.dto.BenutzerDTO;
+import de.hrw.swep.biblio.persistence.dto.BuchDTO;
 
 /**
  * Testklasse fuer den Datenbankzugriff
+ * 
  * @author M. Friedrich
  *
  */
 public class DAOTest {
 
   IDatabaseTester databaseTester;
-  
+
   /**
    * Bringt die Datenbank in einen definierten Ausgangszustand
-   * @throws Exception 
+   * 
+   * @throws Exception
    */
   @Before
   public void setup() throws Exception {
     databaseTester = new JdbcDatabaseTester("org.hsqldb.jdbcDriver",
         "jdbc:hsqldb:file:../biblio-db/database/bibdb", "sa", "");
-    databaseTester.setSetUpOperation(new HsqlDatabaseOperation());
+    // databaseTester.setSetUpOperation(new HsqlDatabaseOperation());
     databaseTester.setDataSet(new FlatXmlDataSetBuilder().build(new File("full.xml")));
     databaseTester.onSetup();
+    
   }
-
+  DAO dao = new DAO();
   /**
    * Testet das Abrufen eines bestimmten Nutzers nach der Nutzer-ID
    */
   @Test
   public void testGetUserById() {
-    DAO dao = new DAO();
+    
     BenutzerDTO b = dao.getBenutzerById(1);
     assertEquals("Adalbert Alt", b.getName());
     assertEquals("Normal", b.getStatus());
@@ -51,15 +56,24 @@ public class DAOTest {
    */
   @Test
   public void testGetBenutzerByName() {
-    fail();
+    Set<BenutzerDTO> b = dao.getBenutzerByName("Adalbert Alt");
+    assertEquals(1, b.size());
+    BenutzerDTO a = b.iterator().next();
+    assertEquals(1, a.getId());
+    assertEquals("Normal", a.getStatus());
   }
 
-  /** 
+  /**
    * Testet das Abrufen aller Buecher eines Autors
    */
   @Test
   public void testGetBuchByAutor() {
-    fail();
+   Set<BuchDTO> b = dao.getBuchByAutor("Karl Kaos");
+   assertEquals(1, b.size());
+   BuchDTO a = b.iterator().next();
+   assertEquals(1, a.getId());
+   assertEquals("Lost and Found", a.getTitel());
+   assertEquals("Verloren", a.getStatus());
   }
 
   /**
@@ -67,6 +81,10 @@ public class DAOTest {
    */
   @Test
   public void testGetBuchByTitle() {
-    fail();
+    Set<BuchDTO> b = dao.getBuchByTitle("Lost and Found");
+    assertEquals(1, b.size());
+    BuchDTO a = b.iterator().next();
+    assertEquals(1, a.getId());
+    assertEquals("Verloren", a.getStatus());
   }
 }
